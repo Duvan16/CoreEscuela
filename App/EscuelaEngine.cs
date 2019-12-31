44e563dd8807c45ceb82f3bf1df6f7decf012375
Duvan González <duvan4448@gmail.com>
2019-12-31 00:00:28 -0500

    Cargar Evaluaciones

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -26,7 +26,31 @@ namespace CoreEscuela
 
         private void CargarEvaluaciones()
         {
-            throw new NotImplementedException();
+            var lista = new List<Evaluaciones>();
+            foreach (var curso in Escuela.Cursos)
+            {
+                foreach (var asignatura in curso.Asignaturas)
+                {
+                    foreach (var alumno in curso.Alumnos)
+                    {
+                        var rnd = new Random(System.Environment.TickCount);
+                        
+                        for (int i = 0; i < 5; i++)
+                        {
+                            var ev = new Evaluaciones
+                            {
+                                Asignatura = asignatura,
+                                Nombre = $"{asignatura.Nombre} Ev#{i + 1}",
+                                Nota = (float)(5 * rnd.NextDouble()),
+                                Alumno = alumno
+                            };
+                            lista.Add(ev);
+                            
+                        }
+                        alumno.Evaluaciones=lista;
+                    }
+                }
+            }
         }
 
         private void CargarAsignaturas()
